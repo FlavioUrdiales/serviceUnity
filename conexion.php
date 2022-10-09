468c49f0fad1bd15c22c9f16e4fbb7e6722797d3
FlavioUrdiales <108547037+FlavioUrdiales@users.noreply.github.com>
2022-10-08 23:06:14 -0500

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,21 @@
+<?php
+
+$servername = "db4free.net"; // Nombre/IP del servidor
+$database = "dbunity"; // Nombre de la BBDD
+$username = "flaviourdiales"; // Nombre del usuario
+$password = "flaviolp12"; // Contraseña del usuario
+// Creamos la conexión
+$con = mysqli_connect($servername, $username, $password, $database);
+
+    $query = "SELECT * FROM top10 ORDER BY score DESC";
+    $result = mysqli_query($con, $query);
+    $scores = array();
+    while($row = mysqli_fetch_assoc($result)){
+        $scores[] = $row;
+    }
+    echo json_encode($scores);
+    mysqli_close($con);
+
+
+
+?>
